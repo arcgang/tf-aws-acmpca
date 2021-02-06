44e55d909898e6b70ffa82a69df9fad269772d21
Archan <aganguly@in.ibm.com>
2021-02-06 13:36:27 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,55 @@
+resource "aws_s3_bucket" "example" {
+    bucket = "example"
+}
+
+data "aws_iam_policy_document" "acmpca_bucket_access" {
+    statement {
+        actions = [
+        "s3:GetBucketAcl",
+        "s3:GetBucketLocation",
+        "s3:PutObject",
+        "s3:PutObjectAcl",
+        ]
+
+        resources = [
+        aws_s3_bucket.example.arn,
+        "${aws_s3_bucket.example.arn}/*",
+        ]
+
+        principals {
+        identifiers = ["acm-pca.amazonaws.com"]
+        type        = "Service"
+        }
+    }
+}
+
+resource "aws_s3_bucket_policy" "example-root" {
+    bucket = aws_s3_bucket.example.id
+    policy = data.aws_iam_policy_document.acmpca_bucket_access.json
+}
+
+resource "aws_acmpca_certificate_authority" "example" {
+    certificate_authority_configuration {
+        key_algorithm     = "RSA_4096"
+        signing_algorithm = "SHA512WITHRSA"
+
+        subject {
+        common_name = "example.com"
+        }
+    }
+
+    enabled = true
+
+    revocation_configuration {
+        crl_configuration {
+        custom_cname       = "crl.example.com"
+        enabled            = true
+        expiration_in_days = 7
+        s3_bucket_name     = aws_s3_bucket.example.id
+        }
+    }
+
+    type = "ROOT"
+
+    depends_on = [aws_s3_bucket_policy.example]
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "region_primary" {
+    description = "AWS region to deploy"
+    default = "us-east-1"
+}
