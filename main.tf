b3096b05d4caa75c94203448c3dc5f8d9a509f66
Archan <aganguly@in.ibm.com>
2021-02-06 16:50:50 +0100

    added subordinate ca creation

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ resource "aws_s3_bucket" "sample-crl-ag" {
     bucket = "sample-crl-ag"
 }
 
+resource "aws_s3_bucket" "sample-crl-ag-sub" {
+    bucket = "sample-crl-ag-sub"
+}
+
 data "aws_iam_policy_document" "acmpca_bucket_access" {
     statement {
         actions = [
@@ -28,6 +32,35 @@ resource "aws_s3_bucket_policy" "sample-crl-ag" {
     policy = data.aws_iam_policy_document.acmpca_bucket_access.json
 }
 
+data "aws_iam_policy_document" "acmpca_bucket_access" {
+    statement {
+        actions = [
+        "s3:GetBucketAcl",
+        "s3:GetBucketLocation",
+        "s3:PutObject",
+        "s3:PutObjectAcl",
+        ]
+
+        resources = [
+        aws_s3_bucket.sample-crl-ag-sub.arn,
+        "${aws_s3_bucket.sample-crl-ag-sub.arn}/*",
+        ]
+
+        principals {
+        identifiers = ["acm-pca.amazonaws.com"]
+        type        = "Service"
+        }
+    }
+}
+
+resource "aws_s3_bucket_policy" "sample-crl-ag-sub" {
+    bucket = aws_s3_bucket.sample-crl-ag-sub.id
+    policy = data.aws_iam_policy_document.acmpca_bucket_access.json
+}
+
+
+
+
 resource "aws_acmpca_certificate_authority" "example-root" {
     certificate_authority_configuration {
         key_algorithm     = "RSA_4096"
@@ -50,6 +83,34 @@ resource "aws_acmpca_certificate_authority" "example-root" {
     }
 
     type = "ROOT"
+
+    depends_on = [aws_s3_bucket_policy.sample-crl-ag]
+}
+
+
+
+resource "aws_acmpca_certificate_authority" "example-subordinate" {
+    certificate_authority_configuration {
+        key_algorithm     = "RSA_4096"
+        signing_algorithm = "SHA512WITHRSA"
+
+        subject {
+        common_name = "sub.example.com"
+        }
+    }
+
+    enabled = true
+
+    revocation_configuration {
+        crl_configuration {
+        custom_cname       = "crl.sub.example.com"
+        enabled            = true
+        expiration_in_days = 7
+        s3_bucket_name     = aws_s3_bucket.sample-crl-ag.id
+        }
+    }
+
+    type = "SUBORDINATE"
 
     depends_on = [aws_s3_bucket_policy.sample-crl-ag]
 }
